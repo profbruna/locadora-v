5703210e226d878d12d83f92c84ca56dd8625d91
andre_cesar <andre_cesar@10.1.105.21>
2014-11-26 14:58:39 -0200

    24.11.14 - Andre e Lucas - Relatorio de locações incompleto

---
+++ application/views/relatorioLocacoes/listar.php
@@ -0,0 +1,80 @@
+
+<div class="col-md-8 col-md-offset-2">
+    <div class="page-header">
+        <h3> Relatorio Locações </h3>
+    </div>
+</div>
+
+<div class="clearfix"></div>
+<br/>
+
+<div class="col-md-10 col-md-offset-1">
+
+
+    <div class="row box-options">
+        <form method="post" action="<?php echo base_url("relatorioLocacoes/listar") ?>">
+            <div class="form-group row">
+                <div class="col-md-2">
+                    <label>Data inicial:</label>
+                    <input type="text"  id="data" name="data_inicial" class="datepicker form-control">
+
+                </div>
+                <div class="col-md-2">
+                    <label>Data final:</label>
+                    <input type="text"  id="data2" name="data_final" class="datepicker form-control">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-md-12" >
+                    <button type="submit" class="btn btn-primary ">  Listar</button>
+                </div>
+            </div>
+        </form>
+
+
+    </div>
+
+    <table class="table table-hover table-striped" >
+        <thead>
+            <tr>
+                <th> #id </th>
+                <th> Produto </th>
+                <th> Data Devolução </th>
+                <th> Hora Devolução </th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            if ($relatorioLocacoes == null) {
+                
+            } else {
+                foreach ($relatorioLocacoes as $relatorioLocacao):
+                    ?>
+                    <tr>
+                        <td> <?php echo $relatorioLocacao->id ?> </td>
+                        <td> <?php echo $produtos[ $relatorioLocacao->produto_id ]->nome ?> </td>
+                        <td> <?php echo $relatorioLocacao->data_devolucao ?> </td>
+                        <td> <?php echo $relatorioLocacao->hora_devolucao ?> </td>
+                    </tr>
+                    <?php
+                endforeach;
+            }
+            ?>
+        </tbody>
+    </table>
+    <div class="row">
+        <div class="col-xs-6"></div>
+        <div class="col-xs-6">
+            <nav>
+
+                <?php
+                if ($relatorioLocacoes == null) {
+                    
+                } else {
+                    echo $this->pagination->create_links();
+                }
+                ?>
+            </nav>
+        </div>
+    </div>
+</div>
--- assets/personal/js/script.js
+++ assets/personal/js/script.js
@@ -1,18 +1,25 @@
-$(document).ready(function () {
-    
+$(document).ready(function() {
+    $('#data').datepicker({
+        format: "yyyy-mm-dd",
+        language: "pt"
+    });
+    $('#data2').datepicker({
+        format: "yyyy-mm-dd",
+        language: "pt"
+    });
     $(".mask-telefone").mask("(99) 9999-9999");
 
-    $(".btn-voltar").click(function () {
+    $(".btn-voltar").click(function() {
         history.back();
     });
-    
-    $(".btn-modal-deletar").click(function () {
+
+    $(".btn-modal-deletar").click(function() {
         $("#ModalExcluirTitle").html("Deseja realmente excluir <b>" + $(this).data("descricao") + "</b>?");
         $("#ConfirmaExclusao").attr("href", $(this).attr("href"));
         $('#ModalExcluir').modal('show');
         return false;
     });
-    
+
     $(".selectpicker").selectpicker();
 
 });
