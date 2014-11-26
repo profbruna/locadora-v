35496d891708f3f7c9032703fa22982182020c2d
Bandolo <Bandolo@172.20.104.108>
2014-11-26 17:10:20 -0200

    26.11.14 {Amanda, Victor} - Relatorio ABC

---
+++ application/controllers/relatoriosABC.php
@@ -0,0 +1,81 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+/**
+ * Controller :: Clientes
+ * 
+ * @author Amanda Croce, Victor Luz
+ * @package application.controllers
+ */
+class RelatoriosABC extends CI_Controller {
+
+    /**
+     * construct
+     */
+    public function __construct() {
+        parent::__construct();
+        $this->load->model(Array("relatorioABC"));
+    }
+
+    /**
+     * index
+     */
+    public function index() {
+        //redirecionamento para a pagina de listagem quando nao for deninido qual a a?ao
+            $data = Array(
+            "title" => " Relatório Mais Locados ",
+            "view" => "relatoriosABC/listar",
+            "data" => Array(
+                "relatorioABC" => null
+            )
+        );
+        $this->load->view("layouts/default", $data);
+    }
+
+    /**
+     * listar
+     */
+
+    public function listar() {
+
+        $dataInicio = $this->input->post("dataInicio");
+        $dataFim = $this->input->post("dataFim");
+        $data = Array(
+            "title" => " Relatório Produtos mais Locados ",
+            "view" => "relatoriosABC/listar",
+            "data" => Array(
+                "relatoriosABC" => $this->relatorioABC->pegarPorLimiteData($dataInicio, $dataFim)
+            )
+        );
+        $this->load->view("layouts/default", $data);
+    }
+
+       
+
+
+    /**
+     * --------------------------------------------------------------------------
+     * ---------------------------------
+     * ------ M?todos especificos
+     */
+
+    /**
+     * @description M?todo que realiza as valida??es dos campos
+     * 
+     * @param Array $dados
+     * @return boolean
+     */
+    public function validacao($dados) {
+        $this->form_validation->set_rules("quantidade", 'Quantidade', 'required');
+      
+        //executar validações
+        return $this->form_validation->run();
+    }
+
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
